521ee67ef59ae2431c347e713684d74f2e7ed072
CalNightingale <cal.nightingale@gmail.com>
2024-12-10 17:35:51 +0000

    renamed to pydicom3 in src folder

---
+++ src/pydicom3/dicomio.py
@@ -0,0 +1,4 @@
+# Copyright 2008-2018 pydicom authors. See LICENSE file for details.
+"""Many point of entry for pydicom read and write functions"""
+from pydicom3.filereader import dcmread  # noqa: F401
+from pydicom3.filewriter import dcmwrite  # noqa: F401
