06564ed1b77a33a03f1542f52698b12ddbf8d395
CalNightingale <cal.nightingale@gmail.com>
2024-12-13 19:31:00 +0000

    applied Ouwen's conversion patch

--- src/pydicom3/jsonrep.py
+++ src/pydicom3/jsonrep.py
@@ -52,10 +52,21 @@ def convert_to_python_number(value: Any, vr: str) -> Any:
 
     if number_type is None:
         return value
+    
+    def _convert(to_type, value):
+        try:
+            if to_type == float:
+                if ',' in value:
+                    value = value.replace(',', '.')
+            return to_type(value)
+        except Exception as e:
+            return value
 
-    if isinstance(value, list | tuple):
+    if isinstance(value, (list, tuple)):
         return [
-            number_type(v) if v is not None else empty_value_for_VR(vr) for v in value
+            _convert(number_type, v) if v is not None
+            else empty_value_for_VR(vr)
+            for v in value
         ]
 
     return number_type(value)
